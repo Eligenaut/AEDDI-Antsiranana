62e46e815fac0fc94626168ca9e3f022f0ee88e3
Eligenaut <brunobrayane@gmail.com>
2025-11-14 11:57:51 +0300

    A jour

--- src/admin/page.tsx
+++ src/admin/page.tsx
@@ -4,12 +4,14 @@ import React from 'react';
 import AddAuthorizedEmail from '../../components/loginComponents/AddAuthorizedEmail.jsx';
 
 export default function AdminEmailsPage() {
+  const handleEmailAdded = () => {};
+
   return (
     <div className="min-h-screen bg-gray-50">
       <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
         <div className="px-4 py-6 sm:px-0">
           <div className="border-4 border-dashed border-gray-200 rounded-lg">
-            <AddAuthorizedEmail />
+            <AddAuthorizedEmail onEmailAdded={handleEmailAdded} />
           </div>
         </div>
       </div>
