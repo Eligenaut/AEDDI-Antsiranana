b4a07dc399e5584096200d8f41adf0aaf6bf6d78
Eligenaut <brunobrayane@gmail.com>
2026-02-15 19:25:55 +0100

    A jour

--- src/admin/page.tsx
+++ src/admin/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React, { useState } from 'react';
 import AddAuthorizedEmail from '../../components/loginComponents/AddAuthorizedEmail.jsx';
 
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React, { useState } from 'react';
 import AddAuthorizedEmail from '../../../components/loginComponents/AddAuthorizedEmail.jsx';
 
@@ -19,7 +17,6 @@ export default function AdminEmailsPage() {
               onClose={() => setIsOpen(false)}
               onEmailAdded={handleEmailAdded}
             />
-
           </div>
         </div>
       </div>
