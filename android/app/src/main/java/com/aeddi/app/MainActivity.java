ce6e2f535be3faa5be7e163c683ad1c2e8f19808
Eligenaut Brayane <brunobrayane@gmail.com>
2026-03-20 09:15:28 +0300

    A jour

---
+++ android/app/src/main/java/com/aeddi/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.aeddi.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aeddi.app',
+  appName: 'AEDDI',
+  webDir: 'public'
+};
+
+export default config;
