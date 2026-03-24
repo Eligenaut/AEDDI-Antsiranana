2ea2be516bc38b656760f5a705e1e145276fb9b5
Eligenaut Brayane <brunobrayane@gmail.com>
2026-03-24 09:46:27 +0300

    A jour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NotificationProvider from "../../components/NotificationProvider";
+import "../config/firebase";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
